b94e883568ee258d177b29c989104a5c6e06d667
thamcthph16766 <thamcthph16766@fpt.edu.vn>
2022-03-11 11:17:13 +0700

    buổi 3

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,44 +2,42 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+
+
 function App() {
   const [count, setCount] = useState(0)
+  const [color, setColor] = useState("green");
+  const [myStatus, setMyStatus] = useState(false);
+  const [product, setProduct] = useState([{id: 1, name: "A"}, {id: 2, name: "B"}])
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+  const removeItem = (id) => {
+    const newProducts = product.filter(item => item.id !== id);
+    setProduct(newProducts);
+  }
+
+  const changeCount = () => {
+    setCount(1)
+  }
+  const changeStatus = () => {
+    setMyStatus(true)
+  }
+
+  return <div>
+    
+    String:  <div style={{background: color, with:100, height:100}}>Cpntent</div> <br/>
+    Boolean: {myStatus ? "Da ket hon" : "Chua ket hon"} <br/>
+    <hr/>
+
+    <h2>Demo event</h2>
+    Number: {count} <br/>
+    <button onClick={() => setCount(count+1)}> Click </button>
+    <button onClick={() => setMyStatus(!myStatus)}> Status </button>
+
+    
+    {myStatus && <div>
+      Arr: {product.map(item => item.name)} </div>}
+      <button onClick={() => removeItem(item.id)}> Delete </button>
     </div>
-  )
 }
 
 export default App
--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -3,21 +3,25 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-const person = {
-  name: "Dat",
-  age: 20,
-  status: true,
-  children: [
-    { id: "1", name: "Tung"},
-    { id: "2", name: "Kien"},
-  ]
-}
-const showAge = (age) => <p>Tuoi cua ban la: {age}</p>
+// const person = {
+//   name: "Dat",
+//   age: 20,
+//   status: true,
+//   children: [
+//     { id: "1", name: "Tung"},
+//     { id: "2", name: "Kien"},
+//   ]
+// }
+// const showAge = (age) => <p>Tuoi cua ban la: {age}</p>
 
 
-const ShowAge = props => <p>Tuoi cua ban la: {props.age}</p>
+// const ShowAge = props => <p>Tuoi cua ban la: {props.age}</p>
 
-ReactDOM.render(<div>
+ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+/* <div>
   <p>Ten: {person.name} </p>
   <p>Tuoi: {person.age} </p>
   <p>{person.status ? "Da ket hon" : "Chua ket hon"}</p>
@@ -28,4 +32,5 @@ ReactDOM.render(<div>
   </ul>
   <div>function: { showAge(person.age) }</div>
   <div>Component: <ShowAge age={person.age} name="Dat" /></div>
-</div>, document.querySelector('#root'));
+</div>,  */
+document.querySelector('#root'));
